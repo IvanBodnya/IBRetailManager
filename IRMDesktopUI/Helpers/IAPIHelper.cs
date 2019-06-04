f1dc534ebeb8963506d662fa67f4f0208f268a66
IvanBodnya <ivanbodnya@gmail.com>
2019-06-04 10:51:19 +0300

    LogIn Form wiring with API
    Connected login form with webapi. Gets back the token or exeption if failed.

---
+++ IRMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using IRMDesktopUI.Models;
+
+namespace IRMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
--- IRMDesktopUI/ViewModels/LoginViewModel.cs
+++ IRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using IRMDesktopUI.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace IRMDesktopUI.ViewModels
     {
         private string _userName;
         private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
 
         public string UserName
         {
@@ -47,9 +54,16 @@ namespace IRMDesktopUI.ViewModels
             }
         }
 
-        public void LogIn(string userName, string password)
+        public async Task LogIn()
         {
-            Console.WriteLine();
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
 
